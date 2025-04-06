c1b9e0843e8644e8e919de175779d402bea63841
CEE <archmagece@users.noreply.github.com>
2025-04-06 09:14:55 +0900

    delete nextcloud. 복잡해서

--- charts/nextcloud/files/dir-data.config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$directory_data = getenv('NEXTCLOUD_PATH_DATA');
-if (! $directory_data) {
-  $directory_data = OC::$SERVERROOT.'/data';
-}
-$CONFIG = array (
-  'datadirectory' => $directory_data,
-);
